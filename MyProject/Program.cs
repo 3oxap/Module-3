4be9b234aed0e3137eb1b13074e88633b6b0d683
3oxap <62433200+3oxap@users.noreply.github.com>
2021-01-07 00:25:35 +0300

    ProjectConsole

---
+++ MyProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args, new Dictionary<string, string>
+            {
+                ["-Name"]="Name"    
+            });
+            var config = builder.Build();
+            var line= DateLibrary.Class1.Output(config["Name"]);
+            Console.WriteLine(line);
+            Console.ReadLine();
+        }
+    }
+}
